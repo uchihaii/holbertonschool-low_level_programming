eacf625206ecef91a893e144a955563d6756497d
uchihaii <idosorior@gmail.com>
2021-10-01 20:52:29 -0500

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-#include <studio.h>
-int main (void)
-{
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
-}
+#include <stdio.h>
 
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+	
+
+		return (0);
+}
