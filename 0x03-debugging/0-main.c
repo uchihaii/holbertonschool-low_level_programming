ec56016ace8e2d782d4d9148c3ce83c662d3be52
uchihaii <idosorior@gmail.com>
2021-10-07 22:12:15 -0500

    0 main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main  the function determines if the integer it prints is positive or negative 
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+      int i;
+      
+      i = 0;
+      positive_or_negative(i);
+
+
+      return (0);
+}
+
