24f89ed83e503ac56dadbd0d6fb1b6512f609ee9
uchihaii <idosorior@gmail.com>
2021-10-08 17:00:16 -0500

    isupper

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  * _isdigit - check the code
+ *   * @c: variable checked against in function (defined in main.c)
+ *    * Return: Always 0.
+ *     */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
