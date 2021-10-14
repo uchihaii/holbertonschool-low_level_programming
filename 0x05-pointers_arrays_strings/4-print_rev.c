7bd6c6b666a43dcc2295ff2c57f2d38a9dc117e0
uchihaii <idosorior@gmail.com>
2021-10-13 23:03:05 -0500

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *  _strlen - returns  length of string
+ *  @s: a string 
+ *  Return: i
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+{
+	i++;
+}
+  return (i); 
+  }
+
+/**
+ *  print_rev - prints string in reverse 
+ *  @s: a strings
+ */
+
+void print_rev(char *s)
+{
+	int i, len = 0;
+	len = _strlen(s);
+
+	for(i = (len - 1); i >=0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
