1a8ec0105a9412b2b0c4760d117560a546f0d6c1
uchihaii <idosorior@gmail.com>
2021-10-13 22:47:56 -0500

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * * _strlen - returns length of string
+ * *@s: a string
+ * *Return: i
+ * */
+
+int _strlen(char *s)
+{
+		int i;
+
+			for (i = 0; s[i] != '\0'; i++)
+					{
+
+							}
+				return (i);
+}
+
