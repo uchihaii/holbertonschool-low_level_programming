2eb3cc44ef86ec9815570fac841dcbc4fe49feac
ivan osorio <91099001+uchihaii@users.noreply.github.com>
2021-10-14 16:47:16 -0500

    puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main -  entrie ponit
- * Return: int
- */ 
+* main -  entrie ponit
+* Return: int
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
