3684a2abbae44543cd95e9e24f73e0eac85f1081
uchihaii <idosorior@gmail.com>
2021-10-13 22:37:03 -0500

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * * swap_int - swaps integers
+ * *@a: any integer
+ * *@b: an integer
+ * */
+
+void swap_int(int *a, int *b)
+{
+		int oriA;
+
+			oriA = *a;
+
+				*a = *b;
+					*b = oriA;
+}
