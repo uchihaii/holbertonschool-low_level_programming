dfeaaa76c670154c9085e587766bec6a1d5844b6
uchihaii <idosorior@gmail.com>
2021-10-14 20:35:53 -0500

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *@dest: string
+ *@src: string
+ *Return: dest
+ */
+
+char *_strcat( char *dest, char *src)
+{
+	int i = 0; j = 0;
+
+	for (; des[i]; i++) /* does not need the != '0'*/
+	{
+	}
+	for (; src[j]; j++)
+		dest[i++] = src[j];
+
+	return (dest);
+}
+
