992b38059c19128a1cb556ca100c45728008435d
uchihaii <idosorior@gmail.com>
2021-10-14 21:24:52 -0500

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+   * _strcmp - entry point
+    * @s1:char
+     * @s2:char
+      * Return:char
+       */
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0') && (*s2 != '\0'))
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+			continue;
+		}
+		else if (*s1 != *s2)
+			return (*s1 - *s2);
+	}
+	return (0);
+}
+
