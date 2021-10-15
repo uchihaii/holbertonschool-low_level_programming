e7cccaae56e7ddaa619406e87799656007aa3c9e
uchihaii <idosorior@gmail.com>
2021-10-14 22:11:20 -0500

    infinite aadd

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,45 @@
+nclude "main.h"
+
+/**
+   * infinite_add - adds two numbers.
+    * @n1:char
+     * @n2:char.
+      * @r:char
+       * @size_r:int
+       * Return:char.
+	 */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j, k, h, m = 0, n;
+
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, h = 0; h < k; k--, h++)
+	{
+		m = r[k];
+		r[k] = r[h];
+		r[h] = m;
+	}
+	return (r);
+}
