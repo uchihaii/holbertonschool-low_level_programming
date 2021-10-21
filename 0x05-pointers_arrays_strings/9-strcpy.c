00ac4db0427f82c448b15382a4ff9f05926d4858
uchihaii <idosorior@gmail.com>
2021-10-21 16:16:27 -0500

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcpy - swap
+ *
+ * @src: pointer
+ *
+ * @dest: pointer
+ *
+ * Return: pointer.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+
+	dest[a] = '\0';
+
+		return (dest);
+}
