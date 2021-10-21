d4c934b1fd4a6164322877f1742f9b85bc8a46dd
uchihaii <idosorior@gmail.com>
2021-10-20 23:29:40 -0500

    pow recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - x pown y
+ * @x: x base
+ * @y: y pot
+ * Return: x pot y
+ */
+int _pow_recursion(int x,  int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x *  _pow_recursion(x, y - 1));
+}
