4ee775cb1c6a9f4ba3abb350b1fafd72ff6ce4e3
uchihaii <idosorior@gmail.com>
2021-10-21 01:28:55 -0500

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,24 @@
-#include ""
+#include "main.h"
+/**
+  * print_alphabet_x10 - show letter the alphabet 10 times
+  *
+  * Return: 0 if correct
+  */
+
+void print_alphabet_x10(void)
+{
+	int a = 97;
+	int b = 1;
+
+	while (b <= 10)
+	{
+		while (a <= 122)
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar(10);
+		b++;
+		a = 97;
+	}
+}
